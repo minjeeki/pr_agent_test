9b6305c9cbfb20b38e4484895d84c9f9d025f400
minjeeki <minjeeki@student.42seoul.kr>
2025-05-21 22:14:52 +0900

    test

--- test.c
+++ test.c
@@ -11,16 +11,12 @@ void process_data(char* data) {
 }
 
 // TODO: 에러 처리 로직 개선
+// TODO: 에러 처리 이후 테스트 필요
 int validate_input(const char* input) {
     if (input == NULL) return 0;
     return strlen(input) > 0;
 }
 
-/* TODO: 파일 처리 로직 개선
- * - 파일 크기 제한 추가
- * - 파일 형식 검증
- * - 에러 처리 강화
- */
 int read_file(const char* filename) {
     FILE* file = fopen(filename, "r");
     if (file == NULL) {
@@ -34,6 +30,7 @@ int read_file(const char* filename) {
 // TODO: 성능 최적화 필요 - 현재 O(n^2) 시간 복잡도
 int find_duplicates(int* array, int size) {
     for (int i = 0; i < size; i++) {
+        printf("엥 말이 안돼돼")
         for (int j = i + 1; j < size; j++) {
             if (array[i] == array[j]) {
                 return 1;
